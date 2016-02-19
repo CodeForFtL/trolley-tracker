15938c8179a25000fe301070274b6fd1ca18d8e8
adam-s <adamsohn_1@hotmail.com>
2016-02-19 08:12:30 -0500

    Server testing wired up! Mocha / Supertest / Chia / Sinon

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,42 @@
 'use strict';
+/**
+ * @link https://github.com/jsoverson/grunt-env
+ * @link https://github.com/pghalliday/grunt-mocha-test
+ */
 
 module.exports = function(grunt) {
     grunt.initConfig({
+        env: {
+            test: {
+                NODE_ENV: 'test',
+                PORT: 9000
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    require: ['mocha.conf.js'],
+                    timeout: 5000, // Set default mocha spec timeout
+                    clearRequireCache: true // "'mocha' loads your test using require resulting in them being added to the require cache"
+                },
+                src: ['server/**/*.spec.js']
+            }
+        },
+        watch: {
+            js: {
+                options: {
+                    spawn: false,
+                },
+                files: '**/*.js',
+                tasks: ['test']
+            }
+        }
+    });
 
-    })
+    grunt.loadNpmTasks('grunt-env');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('test', ['env:test', 'mochaTest']);
 };
--- mocha.conf.js
+++ mocha.conf.js
@@ -1,1 +1,20 @@
 'use strict';
+
+var chai = require('chai');
+
+// Load Chai assertions
+global.expect = chai.expect;
+
+// Load Sinon
+global.sinon = require('sinon');
+
+// Load Supertest as request
+global.request = require('supertest');
+
+// Load the application
+global.app = require(__dirname + '/server.js');
+
+// Initialize Chai plugins
+chai.use(require('sinon-chai'));
+
+
