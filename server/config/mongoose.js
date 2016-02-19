4d80f6ac1f3707cd9cb8d034e069616caa31cade
adam-s <adamsohn_1@hotmail.com>
2016-02-19 09:08:44 -0500

    Give Heroku instance proper production MongoLabs DB instance

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -5,8 +5,8 @@ var config = require('./config'),
 
 module.exports = function () {
     var options = { server: { socketOptions: { keepAlive: 1 } } };
-    //var db = mongoose.connect(config.db, options);
-    var db = null;
+    var db = mongoose.connect(config.db, options);
+
     // Load the application models
     // Here we use require('../models/artist.model'); to load a model. However, this
     // is tightly coupled to a route so it should perhaps be defined in a config file with routes
